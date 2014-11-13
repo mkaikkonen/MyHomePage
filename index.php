3da42e3f743f93ac054e4b64e1507309b198f625
Mika Kaikkonen <mika78_kaikkonen@hotmail.com>
2014-11-13 23:50:40 +0200

    MyHomePageProject

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Mika Kaikkonen - Personal Profile</title>
+        <meta name="description" content="mika,kaikkonen,homepage,hire">
+        <meta name="keywords"  content="mika,kaikkonen,homepage,hire" />
+        <meta name="author" content="Mika Kaikkonen">		
+        <meta name="e-mail" content="mika78_kaikkonen@hotmail.com" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="canonical" href="http://kaikkonen.atspace.eu/" />
+        <!-- <link rel="stylesheet" href="style.css?v=1"> -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-56203231-1', 'auto');ga('send', 'pageview');
+        </script>
+    </head>
+
+    <body>
+        <div id="header" class="container-fluid" >
+          <h1 class="text-center">- Mika Kaikkonen - Personal Profile -</h1>
+        </div>
+        <hr>
+        <div id="core" class="container-fluid">
+            <ul id="myTab" class="nav nav-tabs">
+                <li class="active"><a href="#Me" data-toggle="tab"><span class="glyphicon glyphicon-user"></span> Me</a></li>
+                <li><a href="#Family" data-toggle="tab"><span class="glyphicon glyphicon-heart"></span> Family</a></li>
+                <li><a href="#Work" data-toggle="tab"><span class="glyphicon glyphicon-euro"></span> Work</a></li>
+                <li><a href="#Hobbies" data-toggle="tab"><span class="glyphicon glyphicon-euro"></span> Hobbies</a></li>
+                <li><a href="#About" data-toggle="tab"><span class="glyphicon glyphicon-file"></span> Info</a></li>
+            </ul>
+            <div id="myTabContent" class="tab-content">
+               <div class="tab-pane fade in active" id="Me">
+                    <br>
+                    <h2>Welcome</h2>
+                    <p>
+                    Purpose of this homepage is to give you some
+                    information <b>who I am</b>, <b>where I live</b>,
+                    <b>what I do</b>, <b>what I respect</b> and <b>what are my values</b>.</p>
+                   
+                    <h4>Who I am</h3>
+                    <p> I'm about 36 years and from Finland. I have a family, dogs, hobbies etc. Reason why I made this 
+                        homepage was to learn some HTML5 programming.</p>
+
+                    <h4>Where I live</h4>
+                    <p>I live in Oulu, Ritaharju. You can find more info about Oulu from <a href="http://www.ouka.fi/oulu/english/">City Of Oulu</a> homepage</p>
+
+                    <h4>What I Do</h4>
+                    <p>Most of my free time I want to be with my kids. I play videogames, do legos, read books and go out with them. 
+                        All kind of stuff they want to do.</p>
+                    <p>But sometimes I need my own time. My personal interests are reading, photographing, jogging, cross-country skiing and mountain biking. 
+                        On professional side I'm interested of different programming techonologies and embedded systems.</p>
+
+                    <h4>What I Respect</h4>
+                    <p>Kids, Family, Health, <a href="http://en.wikipedia.org/wiki/Finland">Finland</a>, <a href="http://en.wikipedia.org/wiki/Sisu">Sisu</a>, <a href="http://en.wikipedia.org/wiki/Suomussalmi">Suomussalmi</a>, Raate</p>
+
+                    <h4>What are my values</h4>
+                    <p>Honesty, Attitude, Trust</p>                   
+               </div>
+                <div class="tab-pane fade" id="Family">
+                    <br>
+                    <br>
+                    <h2>My Family</h2>                    
+                    <p>It is big, it is noisy, it takes my nerves 
+                    right to the next century, but <b>I love my family</b>.</p>
+                    <h4>Kids</h4>
+                    <p>I have three kids. Joonatan is 9 years, Jenny is 7 years and Jeremi is 6 years.</p>
+                    <h4>Wife</h4>
+                    <p>My wife is a Nurse. Sometimes she takes care of me when I'm ill.</p>
+                    <h4>Other creatures</h4>
+                    <p>We have two dogs, <a href="http://en.wikipedia.org/wiki/Affenpinscher">Affenpinschers.</a></p>                    
+                    <p>We have one snake, <a href="http://en.wikipedia.org/wiki/Corn_snake">Corn Snake.</a></p>                  
+                </div>
+                <div class="tab-pane fade" id="Hobbies">
+                    <br>
+                    <br>
+                    <h2>My Hobbies</h2>                    
+                    <ul class="list-group">
+                        <li class="list-group-item">Fishing</li>
+                        <li class="list-group-item">Photographing</li>
+                        <li class="list-group-item">Jogging</li>
+                        <li class="list-group-item">
+                           <span class="badge">New</span>
+                           Disc golf
+                        </li>
+                        <li class="list-group-item">Mountain biking</li>
+                        <li class="list-group-item">Downhill skiing</li>
+                        <li class="list-group-item">
+                           <span class="badge">New</span>
+                           Downhill skiing
+                        </li>
+                    </ul>
+                </div>
+                <div class="tab-pane fade" id="Work">
+                    <section id="left" class="col-sm-8">
+                        <br>
+                        <h2><b>Currently I do not have a job!!! I'm looking a new one</b></h2>
+
+                        <h4>Profile</h4>
+                        <p>I am experienced software engineer worked over 12 years with 
+                            baseband software, protocols and protocol stacks and windows 
+                            software in embedded and desktop systems. I've been worked as 
+                            senior designer and specialist in R&D team, and developer/project 
+                            manager in subcontracting projects as mell.</p>
+                        <h4>Specialties</h4>
+                        <p>My personal asset is versatile experience from many software 
+                            projects where C (OOC), C++, C# and Visual Basic have been the main 
+                            development languages. I'm familiar with different software development environments, 
+                            issue tracking and version control tools. While the coding is what I like most,
+                            debugging, log analysis, error hunting, unit testing, and customer support are 
+                            areas where I'm very strong. In addition I have experience from mentoring, 
+                            team leading and project management tasks.</p>
+                        <h4>Personality</h4>
+                        <p>My personality is open and positive, and I like to work both in team and 
+                            independently depending of the work task. When doing programming, I aim 
+                            to produce error free and easy-to-read code that is tested properly. 
+                            I'm interested of software patterns and how to apply them when solving 
+                            programming problems. I'm ready to stretch, and willing to take 
+                            responsibility, and open minded for international work and traveling.</p>
+
+                        <p>If you are interested to hire me, please do not hesitate to contact me via LinkedIn,
+                            email or by phone!</p>
+                    </section>
+                    <section id="right" class="col-sm-4">
+                        <p><img src="mypic.jpg" alt="Mika Kaikkonen"></p>				
+                        Address:       Oulu, Finland<br>
+                        Date of birth: 26-Sep-1978<br>
+                        Email:         mika78_kaikkonen@hotmail.com<br>
+                        Mobile:       +358504821738<br>
+                        <a href="http://fi.linkedin.com/pub/mika-kaikkonen/b/568/b10">
+                        <img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_viewmy_160x25.png" 
+                        width="160" height="25" border="0" alt="View Mika Kaikkonen's profile on LinkedIn"></a>
+                    </section>                            
+                </div>
+                <div class="tab-pane fade" id="About">
+                    <br>
+                    <div class="media-body">
+                      <h2 class="media-heading">These tools and technologies were used in this homepage project.</h2>                      
+                      <div class="media">
+                        <a class="pull-right" href="https://netbeans.org/">
+                            <img src="icon_newsletter_110X32_80FCS.png" height="50" border="0" alt="Netbeans">
+                        </a>
+                        <div class="media-body">
+                          <h4 class="media-heading">NetBeans IDE</h4>
+                          The Smarter and Faster Way to Code. 
+                          Quickly and easily develop desktop, mobile and web applications with Java, HTML5, PHP, C/C++ and more.
+                          NetBeans IDE is FREE, open source, and has a worldwide community of users and developers.
+                        </div>
+                      </div>                         
+                      <div class="media">
+                        <a class="pull-right" href="http://getbootstrap.1com/">
+                          <img src="bootstrap-solid.svg" width="50" height="50" border="0" alt="Bootstrap">
+                        </a>
+                        <div class="media-body">
+                          <h4 class="media-heading">Bootstrap</h4>
+                          Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.
+                        </div>
+                      </div>
+                      <div class="media">
+                        <a class="pull-right" href="http://www.w3.org/TR/html5/">
+                          <img src="HTML5_Logo.svg" width="50" height="50" border="0" alt="HTML5">
+                        </a>
+                        <div class="media-body">
+                          <h4 class="media-heading">HTML5</h4>
+                          HTML5 is the latest HTML standard. It walks hand in hand with CSS3, the latest CSS standard.
+                        </div>
+                      </div>
+                      <div class="media">
+                        <a class="pull-right" href="http://glyphicons.com/">
+                          GLYPHICONS
+                        </a>
+                        <div class="media-body">
+                          <h4 class="media-heading">GLYPHICONS</h4>
+                          GLYPHICONS is a library of precisely prepared monochromatic icons and symbols, created with an emphasis on simplicity and easy orientation.
+                        </div>
+                      </div>                     
+                    </div>
+                </div>
+            </div> 
+        </div>
+        <hr>
+        <div id="footer" class="container-fluid">
+            <p>(c) Mika Kaikkonen 2014. This space was provided by the <a href="http://www.atspace.com/">Atspace.com</a></p>
+            <p>
+                    <?php
+                    $date = date("d.m.Y");
+                    print "Date: ".$date;
+                    ?>					
+            </p>
+        </div>       
+        <!-- Optional: Include the jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Optional: Incorporate the Bootstrap JavaScript plugins -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script>
+             $(function(){
+                $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+                // Get the name of active tab
+                var activeTab = $(e.target).text(); 
+                // Get the name of previous tab
+                var previousTab = $(e.relatedTarget).text(); 
+                $(".active-tab span").html(activeTab);
+                $(".previous-tab span").html(previousTab);
+             });
+          });
+        </script>             
+    </body>
+</html>
